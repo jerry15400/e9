6d3c077d5c393caf615241e6af2b9f1ec18228fc
jerry15400 <166424006+jerry15400@users.noreply.github.com>
2024-04-10 16:19:25 +0800

    Add files via upload

--- tc.sh
+++ tc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [$# -ne 3]; then
+if [ $# -ne 3 ]; then
     echo "Usage: $0 <interface> <file.o> <1|0>"
     exit 1
 fi
@@ -9,10 +9,10 @@ INTERFACE=$1
 FILE=$2
 ACTION=$3
 
-if ["$ACTION" -eq 1]; then
+if [ "$ACTION" -eq 1 ]; then
     tc qdisc add dev $INTERFACE clsact
     tc filter add dev $INTERFACE egress bpf direct-action obj $FILE sec prog
-elif ["$ACTION" -eq 0]; then
+elif [ "$ACTION" -eq 0 ]; then
     tc qdisc del dev $INTERFACE clsact
 else
     echo "Invalid action: $ACTION (must be 1 or 0)"
